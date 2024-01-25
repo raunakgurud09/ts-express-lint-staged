c846613b49f2fde09e8139607844ce3d5670b0c8
Raunak Gurud <raunakgurud2121@gmail.com>
2024-01-25 22:33:14 +0530

    fix: tsc issues

--- src/validations/mongodb.validators.ts
+++ src/validations/mongodb.validators.ts
@@ -5,7 +5,7 @@ import { body, param } from "express-validator";
  * @param {string} idName
  * @description A common validator responsible to validate mongodb ids passed in the url's path variable
  */
-export const mongoIdPathVariableValidator = (idName) => {
+export const mongoIdPathVariableValidator = (idName: string) => {
   return [
     param(idName).notEmpty().isMongoId().withMessage(`Invalid ${idName}`),
   ];
@@ -16,6 +16,6 @@ export const mongoIdPathVariableValidator = (idName) => {
  * @param {string} idName
  * @description A common validator responsible to validate mongodb ids passed in the request body
  */
-export const mongoIdRequestBodyValidator = (idName) => {
+export const mongoIdRequestBodyValidator = (idName: string) => {
   return [body(idName).notEmpty().isMongoId().withMessage(`Invalid ${idName}`)];
 };
